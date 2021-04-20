90b0ff9b5e7ac52d39b5eedfa29bf884a5c3cadc
monika <monikafebrina0602@gmail.com>
2021-04-20 15:25:36 +0700

    Initial sharing of project

---
+++ Main.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+public static void main(String args[])
+    {
+       System.out.println(Calculator.info);
+       
+       Calculator calculator1 = new Calculator();
+       Calculator calculator4 = new Calculator();
+       Calculator calculator2 = new Calculator(3,5);
+       Calculator calculator3 = new Calculator(3);
+       
+       System.out.println("Nilai awal setelah konstruktor dipanggil");
+       System.out.println("Nilai op2 Constructor tanpa parameter: "+calculator1.op2);
+       System.out.println("Nilai op2 Constructor dengan 2 parameter: "+calculator2.op2);
+       System.out.println("Nilai op2 Constructor dengan 3 parameter: "+calculator3.op2);
+       
+       System.out.println("Nilai op1 Constructor tanpa parameter: "+calculator1.getOp1());
+       System.out.println("Nilai op1 Constructor dengan 2 parameter: "+calculator2.getOp1());
+       System.out.println("Nilai op1 Constructor dengan 3 parameter: "+calculator3.getOp1());
+       
+       System.out.println("");
+       System.out.println(Calculator.jumlahobject);
+       
+       System.out.println("");
+       double hasil1 = calculator1.jumlah();
+       double hasil2 = calculator1.jumlah(3.4, 5.4);
+       System.out.println("Hasil penjumlahan method jumlah tanpa parameter: "+ hasil1);
+       System.out.println("Hasil penjumlahan method jumlah dengan parameter: "+ hasil2);
+       
+       System.out.println("");
+       double kurang1 = calculator1.kurang();
+       double kurang2 = calculator1.kurang(3.4, 5.4);
+       System.out.println("Hasil pengurangan method kurang tanpa parameter:  " + kurang1);
+       System.out.println("Hasil pengurangan  method kurang dengan parameter:  "+ kurang2);
+       
+    }
+    
+}
+
